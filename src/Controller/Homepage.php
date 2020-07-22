052f52c686f2b88e4c20c3c2873dbea13d4a649b
Besson Benjamin <bbesson@passion-web.info>
2020-07-22 11:30:48 +0300

    commit 11h30 ajout twig et debug

--- src/Controller/Homepage.php
+++ src/Controller/Homepage.php
@@ -3,34 +3,20 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class Homepage
 {
-    public function homepage(){
-        return new Response("here we are ");
+    public function homepage(Environment $twig){
+        
+        $content = $twig->render('Advert/base.html.twig');
+
+        return new Response($content);
         
     
     }
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Bienvenue sur ma première  !</title>
-  <meta charset="UTF-8">
-</head>
-<body>
-  <h1>Hello World !</h1> 
-   <!-- <img src="">  -->
-  <p>
-    Le Hello World est un grand classique en programmation.
-    Il signifie énormément, car cela veut dire que vous avez
-    réussi à exécuter le programme pour accomplir une tâche simple :
-    afficher ce hello world !
-  </p>
 
-
-</body>
-</html>
 
